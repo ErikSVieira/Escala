596517d152fb9956df817e36b28ac39ce48fb3f1
ErikSVieira <eriksvbh@gmail.com>
2023-06-05 10:52:11 -0300

    update route

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,29 @@
 <?php
 
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\PositionController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Position
+Route::any('/position/search', [PositionController::class, 'search'])->name('position.search');
+Route::get('/position', [PositionController::class, 'index'])->name('position.index');
+Route::get('/position/create', [PositionController::class, 'create'])->name('position.create');
+Route::post('/position/store', [PositionController::class, 'store'])->name('position.store');
+Route::get('/position/{id}', [PositionController::class, 'show'])->name('position.show');
+Route::get('/position/edit/{id}', [PositionController::class, 'edit'])->name('position.edit');
+Route::put('/position/{id}', [PositionController::class, 'update'])->name('position.update');
+Route::delete('/position/{id}', [PositionController::class, 'destroy'])->name('position.destroy');
+
+// Employee
+Route::any('/employee/search', [EmployeeController::class, 'search'])->name('employee.search');
+Route::get('/employee', [EmployeeController::class, 'index'])->name('employee.index');
+Route::get('/employee/create', [EmployeeController::class, 'create'])->name('employee.create');
+Route::post('/employee/store', [EmployeeController::class, 'store'])->name('employee.store');
+Route::get('/employee/{id}', [EmployeeController::class, 'show'])->name('employee.show');
+Route::get('/employee/edit/{id}', [EmployeeController::class, 'edit'])->name('employee.edit');
+Route::put('/employee/{id}', [EmployeeController::class, 'update'])->name('employee.update');
+Route::delete('/employee/{id}', [EmployeeController::class, 'destroy'])->name('employee.destroy');
