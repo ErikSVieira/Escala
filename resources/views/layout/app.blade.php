270a752d792016ee02501d48452ca1ee3cb00940
ErikSVieira <eriksvbh@gmail.com>
2023-06-29 20:17:57 -0300

    update layout new style with bootstrap

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -10,19 +10,32 @@
     {{-- Styles --}}
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
+    <!-- BootStrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
+
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
 </head>
-<body>
+<body class="container-lg bg-body-secondary">
     {{-- Menu principal / Nav --}}
-    @include('layout.nav')
+    <header class="mb-2">
+        @include('layout.nav')
+    </header>
 
-    <main>
+    <section class="mb-2">
         @yield('content')
-    </main>
+    </section>
 
     {{-- Roda pé / Footer --}}
-    @include('layout.footer')
+    <footer class="bg-warning mb-2">
+        @include('layout.footer')
+    </footer>
+
+    <!-- Script -->
+    <script type="text/javascript" src="js/button.js"></script>
 </body>
 </html>
--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,19 +1,35 @@
-<nav>
-    <a href="/">
-        Home
-    </a>
-
-    |
-
-    <a href="{{ route('position.index') }}">
-        Position
-    </a>
-
-    |
-    
-    <a href="{{ route('position.create') }}">
-        Employees
-    </a>
-    
-    <hr>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container-fluid">
+        <img class="logo img-fluid img-thumbnail" src="https://laravel.com/img/logotype.min.svg"
+            alt="Logo da instituição">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="{{ route('home.index') }}">
+                        Home
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('position.index') }}">
+                        Position
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('employee.index') }}">
+                        Employees
+                    </a>
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="btn text-light btn-outline-danger" href="#"><i
+                            class="bi bi-power"></i> Sair</a>
+                </li>
+            </ul>
+        </div>
+    </div>
 </nav>
